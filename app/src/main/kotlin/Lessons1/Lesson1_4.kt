ef97cde84b74ae7f9af842df17add6c6391db9fd
GABAPENTIN <adius729@gmail.com>
2025-11-15 18:49:42 +0300

    KSL-1-4 Гагарин  ВРЕМЯ в космосе

---
+++ app/src/main/kotlin/Lessons1/Lesson1_4.kt
@@ -0,0 +1,30 @@
+package org.example.app
+
+fun main() {
+    // Расстояние в миллиметрах
+    val length: Long = 40_868_600_000
+
+    // Возраст в годах
+    val age: Byte = 27
+
+    // Часть дня
+    val partOfDay: Float = 0.075f
+
+    // Секунды в космосе
+    val seconds: Short = 6480
+
+    // Часть года
+    val partOfYear: Double = 2.0547945205479453E-4
+
+    // Апогей орбиты в метрах
+    val apogee: Int = 327000
+
+    // Вывод всех переменных в столбик
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+    
+}
