7e2cb7619c32fee05c072187af82a21d42ecfacc
GABAPENTIN <adius729@gmail.com>
2025-11-15 18:46:51 +0300

    KSL-1-2 Заказы и количество работников

---
+++ app/src/main/kotlin/Lessons1/Lesson1_2.kt
@@ -0,0 +1,26 @@
+package org.example.app.Lessons1
+
+fun main() {
+
+    // Общее количество заказов
+    val tolalNumbersOfOrders = 75
+
+    // Текст с благодарностью
+    val thankMessage = "Спасибо за покупку!"
+
+    // Вывод в консоль значения всех объявленных ранее переменных
+    println(tolalNumbersOfOrders)
+    println(thankMessage)
+
+    // Количество работников интернет-магазина
+    val numberOfEmployees = 2000
+    // println(numberOfEmployees)
+
+    // Один работник уволился
+    // Новое значение количества работников
+    val updateNumbersOfEmployees = 1999
+
+    // Вывод в консоль нового значения количества работников
+    println(updateNumbersOfEmployees)
+
+}
